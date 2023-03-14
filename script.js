855cb6f6751e7297bb32f4a707800687fd5aac4d
Suryakanta Dash <sdash8273@gmail.com>
2023-03-14 10:16:26 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.querySelector('a[href="#about"]').addEventListener('click', function() {
+  document.querySelector('#about').style.display = 'block';
+  document.querySelector('#contact').style.display = 'none';
+});
+
+document.querySelector('a[href="#contact"]').addEventListener('click', function() {
+  document.querySelector('#about').style.display = 'none';
+  document.querySelector('#contact').style.display = 'block';
+});
